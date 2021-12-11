913de18459adf59a30d0c72e4e40bcdda741f405
Simone G <gaspari.simone@gmail.com>
2021-12-11 14:57:10 +0100

    change useEffect with useMemo

--- src/useContract.ts
+++ src/useContract.ts
@@ -1,18 +1,27 @@
 import { ethers } from 'ethers'
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useMemo } from 'react'
 import { Web3Context } from './useWeb3'
 
 export const useContract = <T>({ networks, abi }: any) => {
   const web3State = useContext(Web3Context)
-  const [contract, setContract] = useState<ethers.Contract>()
-  useEffect(() => {
+  // const [contract, setContract] = useState<ethers.Contract>()
+
+  const contract = useMemo(() => {
     if (web3State?.signer && web3State.chainId && abi) {
-      setContract(new ethers.Contract(networks[web3State.chainId]?.address, abi, web3State.signer))
-    } else {
-      setContract(undefined)
+      return new ethers.Contract(networks[web3State.chainId]?.address, abi, web3State.signer) as unknown as T
     }
-  }, [web3State?.signer, networks, abi])
-  return contract as unknown as T
+    return undefined;
+  }, [abi, networks, web3State.chainId, web3State.signer])
+  // useEffect(() => {
+  //   console.log("web3State?.signer, networks, abi changed", web3State?.signer, networks, abi)
+  //   if (web3State?.signer && web3State.chainId && abi) {
+  //     setContract(new ethers.Contract(networks[web3State.chainId]?.address, abi, web3State.signer))
+  //   } else {
+  //     setContract(undefined)
+  //   }
+  // }, [web3State?.signer, networks, abi])
+  // return contract as unknown as T
+  return contract;
 }
 
 export const loadContract = <T>(address: string, abi: any, signer: ethers.providers.JsonRpcSigner) => {
