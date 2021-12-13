f7fb3ac8733abe7c10384498aa11f61f096127d8
guido.dassori@gmail.com <guido.dassori@gmail.com>
2021-12-13 11:44:09 +0100

    test commit

--- src/useContract.ts
+++ src/useContract.ts
@@ -3,6 +3,7 @@ import { useContext, useEffect, useState } from 'react'
 import { Web3Context } from './useWeb3'
 
 export const useContract = <T>({ networks, abi }: any) => {
+  console.log('Prova useContract')
   const web3State = useContext(Web3Context)
   const [contract, setContract] = useState<ethers.Contract>()
   useEffect(() => {
